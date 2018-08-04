b50e883971216f843c0ac5e0bfa28c4b5fb3042d
Sai Chaitanya Ramisetty <rsaichaitanya@gmail.com>
2018-08-05 00:59:42 +0530

    My first commit.

---
+++ src/main/java/com/sasusri/programs/uploaddownloadfile/UploadDownloadFileApplication.java
@@ -0,0 +1,12 @@
+package com.sasusri.programs.uploaddownloadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadDownloadFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadDownloadFileApplication.class, args);
+	}
+}
